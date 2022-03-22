bee3162d297caa70ece04b8b8fbebcb0f72816d1
Nasake <wilfrid.salles-otta@supinternet.fr>
2022-03-22 14:48:19 +0100

    List start

---
+++ tailwind/src/Pages/List.js
@@ -0,0 +1,9 @@
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+export default function List() {
+    return (
+      <h2 className="text-3xl font-bold underline">
+        1 selected
+      </h2>
+    )
+  }

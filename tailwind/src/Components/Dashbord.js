23430b6eaf8b0c2235973e08dd01d126d8f2538c
Nasake <wilfrid.salles-otta@supinternet.fr>
2022-04-07 00:44:36 +0200

    Second part

--- tailwind/src/Components/Dashbord.js
+++ tailwind/src/Components/Dashbord.js
@@ -14,173 +14,209 @@ export default function Dashboard() {
         <div class="flex">
         <div class="first ml-10 ">
         <div class=" flex flex-col  w-11/12 bg-white">
-                        <div class="flex flex-row justify-between ml-10 mt-4">
-                            <h4>8 task completed out of 10</h4>
-                            <p>Show:  <a class="text-blue-400" href='#'>This Week</a></p>
+            <div class="flex flex-row justify-between ml-10 mt-4">
+                <h4>8 task completed out of 10</h4>
+                <p>Show:  <a class="text-blue-400" href='#'>This Week</a></p>
+            </div>
+            <div class="flex items-center  justify-center mt-3 ">
+
+                <div class="h-1 w-96 bg-lime-500 rounded" >
+
+                </div >
+                <div class="h-1 w-40 bg-slate-400 rounded">
+
+                </div>
+
+            </div>
+            <div class="ml-10 mt-4">
+                <h4>23 December, Sunday</h4>
+
+            </div>
+            <div class="flex flex-row items-center justify-around  p-5 border-b-2 mt-1">
+                <div>
+                    <p class="text-slate-400">Sun</p>
+                    <div class="mt-2">
+
+                        <p class="rounded-full text-white bg-blue-500">23</p>
+
+                    </div>
+
+                </div>
+                <div>
+                    <p class="text-sky-400">Mon</p>
+                    <div class="mt-2 text-sky-400">
+
+                        <p>24</p>
+
+                    </div>
+
+                </div>
+                <div>
+                    <p class="text-slate-400">tue</p>
+                    <div class="mt-2">
+
+                        <p class="text-slate-400">25</p>
+
+                    </div>
+
+                </div>
+                <div>
+                    <p class="text-slate-400">wed</p>
+                    <div class="mt-2">
+
+                        <p class="text-slate-400">26</p>
+
+                    </div>
+
+                </div>
+                <div>
+                    <p class="text-slate-400">thu</p>
+                    <div class="mt-2">
+
+                        <p class="text-slate-400">27</p>
+
+                    </div>
+
+                </div>
+                <div>
+                    <p class="text-slate-400">fri</p>
+                    <div class="mt-2">
+
+                        <p class="text-slate-400">28</p>
+
+                    </div>
+
+                </div>
+                <div>
+                    <p class="text-slate-400">sat</p>
+                    <div class="mt-2">
+
+                        <p class="text-slate-400">29</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div>
+                <div class="p-3  ml-9 mr-9 mt-10 bg-white border-2">
+                    <div class="flex justify-between">
+                        <h3>
+                            Send benefit review by Sunday
+                    </h3>
+                        <p class="text-slate-400">Reminder</p>
+
+                    </div>
+                    <p>Due date: December 23, 2018</p>
+                    <div class="flex justify-between mt-2">
+                        <div class="flex items-center mt-3">
+                            <img class="self-center h-7 rounded-full " src="image 2.png" alt="" />
+                            <p class="text-slate-400 ml-3">George Fields</p>
+
                         </div>
-                        <div class="flex items-center  justify-center mt-3 ">
+                        <button class="text-white bg-green-500 w-32 h-7 mt-4 rounded">Completed</button>
 
-                            <div class="h-1 w-96 bg-lime-500 rounded" >
+                    </div>
 
-                            </div >
-                            <div class="h-1 w-40 bg-slate-400 rounded">
+                </div>
 
-                            </div>
 
-                        </div>
-                        <div class="ml-10 mt-4">
-                            <h4>23 December, Sunday</h4>
+                <div class="p-3  ml-9 mr-9 mt-5 bg-white border-2">
+                    <div class="flex justify-between">
+                        <h3>
+                            Send benefit review by Sunday
+                    </h3>
+                        <p class="text-slate-400">Reminder</p>
 
-                        </div>
-                        <div class="flex flex-row items-center justify-around  p-5 border-b-2 mt-1">
-                            <div>
-                                <p class="text-slate-400">Sun</p>
-                                <div class="mt-2">
+                    </div>
 
-                                    <p class="rounded-full text-white bg-blue-500">23</p>
-
-                                </div>
-
-                            </div>
-                            <div>
-                                <p class="text-sky-400">Mon</p>
-                                <div class="mt-2 text-sky-400">
-
-                                    <p>24</p>
-
-                                </div>
-
-                            </div>
-                            <div>
-                                <p class="text-slate-400">tue</p>
-                                <div class="mt-2">
-
-                                    <p class="text-slate-400">25</p>
-
-                                </div>
-
-                            </div>
-                            <div>
-                                <p class="text-slate-400">wed</p>
-                                <div class="mt-2">
-
-                                    <p class="text-slate-400">26</p>
-
-                                </div>
-
-                            </div>
-                            <div>
-                                <p class="text-slate-400">thu</p>
-                                <div class="mt-2">
-
-                                    <p class="text-slate-400">27</p>
-
-                                </div>
-
-                            </div>
-                            <div>
-                                <p class="text-slate-400">fri</p>
-                                <div class="mt-2">
-
-                                    <p class="text-slate-400">28</p>
-
-                                </div>
-
-                            </div>
-                            <div>
-                                <p class="text-slate-400">sat</p>
-                                <div class="mt-2">
-
-                                    <p class="text-slate-400">29</p>
-
-                                </div>
-
-                            </div>
+                    <p>Due date: December 23, 2018</p>
+                    <div class="flex justify-between mt-2">
+                        <div class="flex items-center mt-3">
+                            <img class="self-center h-7 rounded-full " src="image 2.png" alt="" />
+                            <p class="text-slate-400 ml-3">George Fields</p>
 
                         </div>
-                        <div>
-                            <div class="p-3  ml-9 mr-9 mt-10 bg-white border-2">
-                                <div class="flex justify-between">
-                                    <h3>
-                                        Send benefit review by Sunday
-                                </h3>
-                                    <p class="text-slate-400">Reminder</p>
+                        <button class="text-white bg-red-500 w-32 h-7 mt-4 rounded">Ended</button>
 
-                                </div>
-                                <p>Due date: December 23, 2018</p>
-                                <div class="flex justify-between mt-2">
-                                    <div class="flex items-center mt-3">
-                                        <img class="self-center h-7 rounded-full " src="image 2.png" alt="" />
-                                        <p class="text-slate-400 ml-3">George Fields</p>
+                    </div>
 
-                                    </div>
-                                    <button class="text-white bg-green-500 w-32 h-7 mt-4 rounded">Completed</button>
-
-                                </div>
-
-                            </div>
+                </div>
 
 
-                            <div class="p-3  ml-9 mr-9 mt-5 bg-white border-2">
-                                <div class="flex justify-between">
-                                    <h3>
-                                        Send benefit review by Sunday
-                                </h3>
-                                    <p class="text-slate-400">Reminder</p>
+                <div class="p-3  ml-9 mr-9 mt-5 mb-3 bg-white border-2">
+                    <div>
+                        <div class="flex justify-between">
+                            <h3>
+                                Send benefit review by Sunday
+                    </h3>
+                            <p class="text-slate-400">Reminder</p>
 
-                                </div>
-
-                                <p>Due date: December 23, 2018</p>
-                                <div class="flex justify-between mt-2">
-                                    <div class="flex items-center mt-3">
-                                        <img class="self-center h-7 rounded-full " src="image 2.png" alt="" />
-                                        <p class="text-slate-400 ml-3">George Fields</p>
-
-                                    </div>
-                                    <button class="text-white bg-red-500 w-32 h-7 mt-4 rounded">Ended</button>
-
-                                </div>
-
-                            </div>
-
-
-                            <div class="p-3  ml-9 mr-9 mt-5 mb-3 bg-white border-2">
-                                <div>
-                                    <div class="flex justify-between">
-                                        <h3>
-                                            Send benefit review by Sunday
-                                </h3>
-                                        <p class="text-slate-400">Reminder</p>
-
-                                    </div>
-                                    <p>Due date: December 23, 2018</p>
-                                    <div class="flex justify-between mt-2">
-                                        <div class="flex items-center mt-3">
-                                            <img class="self-center h-7 rounded-full " src="image 2.png" alt="" />
-                                            <p class="text-slate-400 ml-3">George Fields</p>
-
-                                        </div>
-                                        <button class="text-white bg-green-500 w-32 h-7 mt-4 rounded">Completed</button>
-
-                                    </div>
-
-                                </div>
-
-                            </div>
                         </div>
-                        <div class="flex items-center justify-center">
-                            <button class="text-sky-400 mb-5">show more</button>
+                        <p>Due date: December 23, 2018</p>
+                        <div class="flex justify-between mt-2">
+                            <div class="flex items-center mt-3">
+                                <img class="self-center h-7 rounded-full " src="image 2.png" alt="" />
+                                <p class="text-slate-400 ml-3">George Fields</p>
+
+                            </div>
+                            <button class="text-white bg-green-500 w-32 h-7 mt-4 rounded">Completed</button>
+
                         </div>
+
+                    </div>
+
+                </div>
+            </div>
+            <div class="flex items-center justify-center">
+                <button class="text-sky-400 mb-5">show more</button>
+            </div>
 
         </div>
         </div>
         
         <div class= 'second'>
-            <div class= 'w-32 h-24 green mb-10' >
-
+            <div class= 'bg-white mb-10 mr-10' >
+            <div class="flex flex-row justify-between">
+                <h4>Deals</h4>
+                <p>Show:  <a class="text-blue-400" href='#'>Monthly</a></p>
             </div>
-            <div class= 'w-14 h-14 red' >
+            <div class="flex flex-row mt-8 mb-4">
+                <p>Closed Deals</p>
+            </div>
+            <div class="flex">
+                <ul class='flex flex-col justify-between'>
+                    <li>200</li>
+                    <li>150</li>
+                    <li>100</li>
+                    <li>50</li>
+                    <li>0</li>
+                </ul>
+                <img src='https://cdn.discordapp.com/attachments/898157938051526656/961378095124873286/unknown.png' alt=''></img>
+            </div>
+            <div>
+                <ul class="flex justify-center justify-between mr-10 ml-10">
+                    <li>1 Dec</li>
+                    <li>8 Dec</li>
+                    <li>16 Dec</li>
+                    <li>31 Dec</li>
+                </ul>
+            </div>
+            </div>
 
+            <div class= 'h-20 bg-white mr-10' >
+            <div class="flex flex-row justify-between">
+                <h4>Tasks</h4>
+                <p>Show:  <a class="text-blue-400" href='#'>This Month</a></p>
+            </div>
+            <div class='flex justify-between mr-10 ml-10 mt-10'>
+                <div class="circle flex justify-center items-center"><p>60%</p></div>
+                <ul class='flex flex-col justify-between mt-6 mb-6'>
+                    <li>Active</li>
+                    <li>Completed</li>
+                    <li>Ended</li>
+                </ul>
+            </div>
             </div>
         
         </div>

1130060af98c55ddc69964851ad9c8d3b85e1a81
TAHATJ48 <tahajotey@gmail.com>
2022-04-06 18:44:05 +0200

    fix

--- tailwind/src/App.js
+++ tailwind/src/App.js
@@ -3,14 +3,14 @@ import Chat from './Pages/Chat';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Dashboard from './Components/Dashbord';
+import Dashboard from './Pages/Dashbord';
 function App() {
   return (
 
     <BrowserRouter>
     < Routes >
       <Route path="/list" element={<List />}> </Route>
-      <Route path="/dashboard" element={<Dashboard />}> </Route>
+      <Route path="/dash" element={<Dashboard />}> </Route>
       <Route path="/chat" element={<Chat/>}>  </Route>
     </Routes >
     </BrowserRouter>
